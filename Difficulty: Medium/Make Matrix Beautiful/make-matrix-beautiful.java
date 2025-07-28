f3bb530c2e67415d8683308cb9ec808b47de1533
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2025-07-28 08:14:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Make Matrix Beautiful/make-matrix-beautiful.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static int balanceSums(int[][] mat) {
+        int n = mat.length;
+        int[] rowSum = new int[n];
+        int[] colSum = new int[n];
+        int total = 0;
+        int maxSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rowSum[i] += mat[i][j];
+                colSum[j] += mat[i][j];
+                total += mat[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            maxSum = Math.max(maxSum, rowSum[i]);
+            maxSum = Math.max(maxSum, colSum[i]);
+        }
+
+        int targetTotal = n * maxSum;
+        int ops = targetTotal - total;
+        return ops;
+    }
+}
