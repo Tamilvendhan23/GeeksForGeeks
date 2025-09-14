d4cd39fc71175ef1b07dba73c6f5b05c80d84bba
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2025-09-14 14:33:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.java
+++ Difficulty: Medium/Gas Station/gas-station.java
@@ -1,69 +1,20 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-
-            ArrayList<Integer> array1 = new ArrayList<Integer>();
-            ArrayList<Integer> array2 = new ArrayList<Integer>();
-
-            String line = read.readLine();
-            String[] tokens = line.split(" ");
-            for (String token : tokens) {
-                array1.add(Integer.parseInt(token));
-            }
-            line = read.readLine();
-            tokens = line.split(" ");
-            for (String token : tokens) {
-                array2.add(Integer.parseInt(token));
-            }
-
-            // ArrayList<Integer> v = new ArrayList<Integer>();
-            int[] gas = new int[array1.size()];
-            int idx = 0;
-            for (int i : array1) gas[idx++] = i;
-
-            int[] cost = new int[array2.size()];
-            idx = 0;
-            for (int i : array2) cost[idx++] = i;
-
-            int ans = new Solution().startStation(gas, cost);
-
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     public int startStation(int[] gas, int[] cost) {
-        int totalGas = 0, totalCost = 0, tank = 0, startIndex = 0;
-
+        int totalGas = 0, totalCost = 0;
+        int start = 0, tank = 0;
+        
         for (int i = 0; i < gas.length; i++) {
             totalGas += gas[i];
             totalCost += cost[i];
             tank += gas[i] - cost[i];
-
-            // If tank becomes negative, reset and move startIndex to next station
+            
+            // If tank is negative, reset start to i+1
             if (tank < 0) {
-                startIndex = i + 1;
+                start = i + 1;
                 tank = 0;
             }
         }
-
-        return (totalGas >= totalCost) ? startIndex : -1;
+        
+        return (totalGas >= totalCost) ? start : -1;
     }
 }
-
