09722d1350432ac2103fa4110bcdde74f77e8675
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2025-09-17 10:54:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.java
+++ Difficulty: Medium/Decode the string/decode-the-string.java
@@ -1,65 +1,28 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-        int t = Integer.parseInt(in.readLine());
-        while (t-- > 0) {
-            String s = in.readLine();
-
-            Solution ob = new Solution();
-            out.println(ob.decodeString(s));
-
-            out.println("~");
-        }
-        out.close();
-    }
-}
-// } Driver Code Ends
-
-
-
 class Solution {
-    public String decodeString(String s) {
-        // Two stacks: one for numbers and one for strings
-        Stack<Integer> numStack = new Stack<>();
-        Stack<StringBuilder> strStack = new Stack<>();
-        StringBuilder currentStr = new StringBuilder();
-        int currentNum = 0;
-        
-        // Iterate through each character in the string
+    static String decodeString(String s) {
+        java.util.Stack<Integer> numStack = new java.util.Stack<>();
+        java.util.Stack<StringBuilder> strStack = new java.util.Stack<>();
+        StringBuilder curr = new StringBuilder();
+        int num = 0;
         for (char c : s.toCharArray()) {
             if (Character.isDigit(c)) {
-                // Build the number digit by digit
-                currentNum = currentNum * 10 + (c - '0');
-            } 
-            else if (c == '[') {
-                // Push current number and string to stacks
-                numStack.push(currentNum);
-                strStack.push(currentStr);
-                // Reset for inner pattern
-                currentNum = 0;
-                currentStr = new StringBuilder();
-            } 
-            else if (c == ']') {
-                // Pop number and previous string
-                int num = numStack.pop();
-                StringBuilder prevStr = strStack.pop();
-                // Multiply current string by number and append to previous string
-                for (int i = 0; i < num; i++) {
-                    prevStr.append(currentStr);
+                num = num * 10 + (c - '0'); // For numbers > 9
+            } else if (c == '[') {
+                numStack.push(num);      // Push current number
+                strStack.push(curr);     // Push current string
+                curr = new StringBuilder();
+                num = 0;
+            } else if (c == ']') {
+                int repeat = numStack.pop();
+                StringBuilder prev = strStack.pop();
+                for (int i = 0; i < repeat; i++) {
+                    prev.append(curr);
                 }
-                currentStr = prevStr;
-            } 
-            else {
-                // Add character to current string
-                currentStr.append(c);
+                curr = prev;
+            } else {
+                curr.append(c);           // Append characters to current string
             }
         }
-        
-        return currentStr.toString();
+        return curr.toString();
     }
 }
