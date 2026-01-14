aa361b414f800a900cc20f69578cdf1f72a65ee9
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2026-01-14 16:36:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Police and Thieves/police-and-thieves.java
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.java
@@ -2,31 +2,25 @@ import java.util.*;
 
 class Solution {
     public int catchThieves(char[] arr, int k) {
-        int n = arr.length;
         List<Integer> police = new ArrayList<>();
-        List<Integer> thief = new ArrayList<>();
-        
-        // Store indices of 'P' and 'T'
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 'P') {
-                police.add(i);
-            } else if (arr[i] == 'T') {
-                thief.add(i);
-            }
+        List<Integer> thieves = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 'P') police.add(i);
+            else thieves.add(i);
         }
 
         int i = 0, j = 0, count = 0;
-        
-        // Greedy matching
-        while (i < police.size() && j < thief.size()) {
-            if (Math.abs(police.get(i) - thief.get(j)) <= k) {
+
+        while (i < police.size() && j < thieves.size()) {
+            if (Math.abs(police.get(i) - thieves.get(j)) <= k) {
                 count++;
                 i++;
                 j++;
-            } else if (thief.get(j) < police.get(i)) {
-                j++;
-            } else {
+            } else if (police.get(i) < thieves.get(j)) {
                 i++;
+            } else {
+                j++;
             }
         }
 
