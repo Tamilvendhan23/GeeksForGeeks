8f8e5d5f393c42bc395831368d2474ef38d1ac53
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2026-03-12 23:41:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.java
+++ Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.java
@@ -1,18 +1,26 @@
 class Solution {
     public int kBitFlips(int[] arr, int k) {
         int n = arr.length;
-        int res = 0, flip = 0;
-        int[] hint = new int[n + 1]; // track where flips end
-
-        for (int i = 0; i < n; i++) {
-            flip += hint[i]; // remove expired flips
-            if ((arr[i] + flip) % 2 == 0) { // bit is 0 after flips
-                if (i + k > n) return -1;   // not enough space to flip
-                res++;
-                flip++;
-                hint[i + k]--; // end effect of this flip
+        int[] flip = new int[n];
+        int curFlips = 0;
+        int ans = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            curFlips += flip[i];
+            if (i >= k) {
+                curFlips -= flip[i - k];
+            }
+            
+            if ((arr[i] ^ (curFlips & 1)) == 0) {
+                if (i + k > n) {
+                    return -1;
+                }
+                ++ans;
+                flip[i] = 1;
+                ++curFlips;
             }
         }
-        return res;
+        
+        return ans;
     }
 }
