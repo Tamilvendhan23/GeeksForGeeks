6a51192ce7b1d16a5ec7a8b17dec2d246c451338
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2026-03-17 17:04:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Burning Tree/burning-tree.java
+++ Difficulty: Hard/Burning Tree/burning-tree.java
@@ -1,210 +1,67 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
 import java.util.*;
-import java.util.LinkedList;
-import java.util.Queue;
-
-class Node {
-    int data;
-    Node left;
-    Node right;
-
-    Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
-
-class GfG {
-
-    static Node buildTree(String str) {
-
-        if (str.length() == 0 || str.charAt(0) == 'N') {
-            return null;
-        }
-
-        String ip[] = str.split(" ");
-        // Create the root of the tree
-        Node root = new Node(Integer.parseInt(ip[0]));
-        // Push the root to the queue
-
-        Queue<Node> queue = new LinkedList<>();
-
-        queue.add(root);
-        // Starting from the second element
-
-        int i = 1;
-        while (queue.size() > 0 && i < ip.length) {
-
-            // Get and remove the front of the queue
-            Node currNode = queue.peek();
-            queue.remove();
-
-            // Get the current node's value from the string
-            String currVal = ip[i];
-
-            // If the left child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the left child for the current node
-                currNode.left = new Node(Integer.parseInt(currVal));
-                // Push it to the queue
-                queue.add(currNode.left);
-            }
-
-            // For the right child
-            i++;
-            if (i >= ip.length) break;
-
-            currVal = ip[i];
-
-            // If the right child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the right child for the current node
-                currNode.right = new Node(Integer.parseInt(currVal));
-
-                // Push it to the queue
-                queue.add(currNode.right);
-            }
-            i++;
-        }
-
-        return root;
-    }
-
-    static void printInorder(Node root) {
-        if (root == null) return;
-
-        printInorder(root.left);
-        System.out.print(root.data + " ");
-
-        printInorder(root.right);
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t > 0) {
-            String s = br.readLine();
-            int target = Integer.parseInt(br.readLine());
-            Node root = buildTree(s);
-
-            Solution g = new Solution();
-            System.out.println(g.minTime(root, target));
-            t--;
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-/*
-class Node {
-    int data;
-    Node left;
-    Node right;
-
-    Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}  */
- 
 
 class Solution {
-    static class NodeParentPair {
-        Node node;
-        Node parent;
-        NodeParentPair(Node node, Node parent) {
-            this.node = node;
-            this.parent = parent;
-        }
-    }
-
-    public static int minTime(Node root, int target) {
+    public int minTime(Node root, int target) {
+        if (root == null) return 0;
+        
+        // First pass: build parent map and find target node
         Map<Node, Node> parentMap = new HashMap<>();
-        Node targetNode = buildParentMapAndFindTarget(root, parentMap, target);
-
-        return bfsToBurnTree(targetNode, parentMap);
-    }
-
-    // Helper to build parent map and find target node
-    private static Node buildParentMapAndFindTarget(Node root, Map<Node, Node> parentMap, int target) {
         Queue<Node> queue = new LinkedList<>();
         queue.offer(root);
+        parentMap.put(root, null);
         Node targetNode = null;
-
+        
         while (!queue.isEmpty()) {
-            Node current = queue.poll();
-            if (current.data == target)
-                targetNode = current;
-
-            if (current.left != null) {
-                parentMap.put(current.left, current);
-                queue.offer(current.left);
+            Node curr = queue.poll();
+            if (curr.data == target) {
+                targetNode = curr;
             }
-
-            if (current.right != null) {
-                parentMap.put(current.right, current);
-                queue.offer(current.right);
+            if (curr.left != null) {
+                parentMap.put(curr.left, curr);
+                queue.offer(curr.left);
+            }
+            if (curr.right != null) {
+                parentMap.put(curr.right, curr);
+                queue.offer(curr.right);
             }
         }
-
-        return targetNode;
-    }
-
-    // BFS to simulate fire spread
-    private static int bfsToBurnTree(Node targetNode, Map<Node, Node> parentMap) {
-        Queue<Node> queue = new LinkedList<>();
+        
+        // Second pass: BFS from target, level by level, marking visited
         Set<Node> visited = new HashSet<>();
         queue.offer(targetNode);
         visited.add(targetNode);
         int time = 0;
-
+        
         while (!queue.isEmpty()) {
-            int size = queue.size();
-            boolean anySpread = false;
-
-            for (int i = 0; i < size; i++) {
-                Node current = queue.poll();
-
-                // Spread to left
-                if (current.left != null && !visited.contains(current.left)) {
-                    visited.add(current.left);
-                    queue.offer(current.left);
-                    anySpread = true;
+            int levelSize = queue.size();
+            boolean hasNext = false;
+            for (int i = 0; i < levelSize; i++) {
+                Node curr = queue.poll();
+                // left
+                if (curr.left != null && !visited.contains(curr.left)) {
+                    queue.offer(curr.left);
+                    visited.add(curr.left);
+                    hasNext = true;
                 }
-
-                // Spread to right
-                if (current.right != null && !visited.contains(current.right)) {
-                    visited.add(current.right);
-                    queue.offer(current.right);
-                    anySpread = true;
+                // right
+                if (curr.right != null && !visited.contains(curr.right)) {
+                    queue.offer(curr.right);
+                    visited.add(curr.right);
+                    hasNext = true;
                 }
-
-                // Spread to parent
-                Node parent = parentMap.get(current);
-                if (parent != null && !visited.contains(parent)) {
-                    visited.add(parent);
-                    queue.offer(parent);
-                    anySpread = true;
+                // parent
+                Node par = parentMap.get(curr);
+                if (par != null && !visited.contains(par)) {
+                    queue.offer(par);
+                    visited.add(par);
+                    hasNext = true;
                 }
             }
-
-            if (anySpread)
+            if (!queue.isEmpty()) {
                 time++;
+            }
         }
-
+        
         return time;
     }
 }
