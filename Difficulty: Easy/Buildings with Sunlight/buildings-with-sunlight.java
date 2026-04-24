2ce4e89d8cb9b11bf301dea6f74af27d57321e83
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2026-04-24 10:01:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Buildings with Sunlight/buildings-with-sunlight.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int visibleBuildings(int[] arr) {
+        if (arr.length == 0) return 0;
+        int count = 1;
+        int maxHeight = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] >= maxHeight) {
+                count++;
+                maxHeight = arr[i];
+            }
+        }
+        return count;
+    }
+}
