184c309c3948055e5c40fb1a16e24bf01d92a4f3
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2026-06-10 07:25:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Binary Searchable Count/binary-searchable-count.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    public int binarySearchable(int[] arr) {
+
+        int n = arr.length;
+        int count = 0;
+
+        for (int target : arr) {
+
+            int l = 0;
+            int r = n - 1;
+            boolean found = false;
+
+            while (l <= r) {
+
+                int mid = l + (r - l) / 2;
+
+                if (arr[mid] == target) {
+                    found = true;
+                    break;
+                }
+
+                else if (arr[mid] < target) {
+                    l = mid + 1;
+                }
+
+                else {
+                    r = mid - 1;
+                }
+            }
+
+            if (found) count++;
+        }
+
+        return count;
+    }
+}
