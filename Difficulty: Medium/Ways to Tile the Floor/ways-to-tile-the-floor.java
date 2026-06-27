0747a2517ffbf9cb3287b2c8827ce3984330f074
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2026-06-27 07:22:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Tile the Floor/ways-to-tile-the-floor.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countWays(int n, int m) {
+        int MOD = 1000000007;
+
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i < m) {
+                dp[i] = 1;
+            } else if (i == m) {
+                dp[i] = 2;
+            } else {
+                dp[i] = (dp[i - 1] + dp[i - m]) % MOD;
+            }
+        }
+
+        return (int) dp[n];
+    }
+}
