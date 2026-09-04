6f14320c00d15f849c97672f5090f70e2472b5ad
TAMILVENDHAN M <147990328+Tamilvendhan23@users.noreply.github.com>
2026-09-04 14:57:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bird and Max Fruit Gathering/bird-and-max-fruit-gathering.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxFruits(ArrayList<Integer> arr, int m) {
+        int n = arr.size();
+
+        long windowSum = 0;
+
+        // Sum of the first m trees
+        for (int i = 0; i < m; i++) {
+            windowSum += arr.get(i);
+        }
+
+        long maxSum = windowSum;
+
+        // Check all remaining circular windows
+        for (int start = 1; start < n; start++) {
+            windowSum -= arr.get(start - 1);
+            windowSum += arr.get((start + m - 1) % n);
+
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return (int) maxSum;
+    }
+}
